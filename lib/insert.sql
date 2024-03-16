c0604035b1f2db4747f4f95124da89b74aeea821
Eric Maranga <ekirira22@gmail.com>
2024-03-16 23:06:13 +0300

    completed assessment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+    VALUES 
+        ('Mr Chocolate', 2, 'M', 'black', 'sassy', 1),
+        ('Rowdy', 2, 'F', 'brown', 'rowdy', 1),
+        ('Tabitha', 5, 'F', 'cream', 'noisy', 1),
+        ('Sergeant Brown', 3, 'M', 'black', 'sickly', 0),
+        ('Melissa', 7, 'F', 'black/white', 'joyful', 1),
+        ('Grinch', 1, 'M', 'green', 'moody', 1),
+        ('Wendy', 3, 'F', 'black', 'angry', 0),
+        (NULL, 4, 'M', 'white', 'mellow', 1);
